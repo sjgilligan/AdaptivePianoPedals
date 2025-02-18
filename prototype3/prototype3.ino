0689775495046191710ea4f3d0846451e6ed13f8
Sebastian Gilligan <sjgillig@bu.edu>
2025-02-17 22:22:19 -0500

    Update prototype3.ino

--- prototype3/prototype3.ino
+++ prototype3/prototype3.ino
@@ -13,36 +13,79 @@ LED LEDStick1;
 
 int buttonInput1 = 0;
 
-control_state_t conState1 = INIT;
+control_state_t conState1 = MIN;
 int potInput1 = A1; //15
 int sensorInput1 = A6; //20
 int motorOutput1 = 14;
 
 int potValue1 = 0;
-int ledValue1 = 0;
+int ledValue1 = -1;
 int last_ledValue1 = 0;
 
-void led_stick(LED *led, control_state_t state, int value){  
+int get_led_value(int pot_value){
+  int led_value;
+
+  switch (pot_value / 100) {
+    case 0:
+        if (pot_value < 2) {
+            led_value = -1;
+        } else {
+            led_value = 0;
+        }
+        break;
+    case 1:
+        led_value = 1;
+        break;
+    case 2:
+        led_value = 2;
+        break;
+    case 3:
+        led_value = 3;
+        break;
+    case 4:
+        led_value = 4;
+        break;
+    case 5:
+        led_value = 5;
+        break;
+    case 6:
+        led_value = 6;
+        break;
+    case 7:
+        led_value = 7;
+        break;
+    case 8:
+        led_value = 8;
+        break;
+    default:
+        led_value = 9;
+        break;
+  }
+  
+  return led_value;
+}
+
+void led_stick(LED &led, control_state_t state, int value){  
   if(value < 0){
     led.LEDOff();
   }
 
   switch(state){
     case MIN:
-      LEDStick1.LEDOff();
-      LEDStick1.setLEDColor(ledValue1, 255, 0, 0);
+      led.LEDOff();
+      led.setLEDColor(value, 255, 0, 0);
       break;
     case MAX:
-      LEDStick1.LEDOff();
-      LEDStick1.setLEDColor(ledValue1, 0, 255, 0);
+      led.LEDOff();
+      led.setLEDColor(value, 0, 255, 0);
       break;
     case SENS:
-      LEDStick1.LEDOff();
-      LEDStick1.setLEDColor(ledValue1, 0, 0, 255);
+      led.LEDOff();
+      led.setLEDColor(value, 0, 0, 255);
       break;
     default:
-      LEDStick1.LEDOff();
-      LEDStick1.setLEDColor(ledValue1, 100, 100, 100);
+      led.LEDOff();
+      led.setLEDColor(value, 100, 100, 100);
       break;
   }
 
@@ -62,66 +105,15 @@ void setup() {
 void loop() {
 
   potValue1 = analogRead(potInput1);
-
-  switch (potValue1 / 100) {
-    case 0:
-        if (potValue1 < 2) {
-            ledValue1 = -1;
-        } else {
-            ledValue1 = 0;
-        }
-        break;
-    case 1:
-        ledValue1 = 1;
-        break;
-    case 2:
-        ledValue1 = 2;
-        break;
-    case 3:
-        ledValue1 = 3;
-        break;
-    case 4:
-        ledValue1 = 4;
-        break;
-    case 5:
-        ledValue1 = 5;
-        break;
-    case 6:
-        ledValue1 = 6;
-        break;
-    case 7:
-        ledValue1 = 7;
-        break;
-    case 8:
-        ledValue1 = 8;
-        break;
-    default:
-        ledValue1 = 9;
-        break;
-}
-
+  ledValue1 = get_led_value(potValue1);
+  Serial.print(ledValue1);
 
   if(last_ledValue1 != ledValue1){
-    Serial.print("change\n");
-    if(ledValue1 == -1){
-      LEDStick1.LEDOff();
-    }else{
-      LEDStick1.LEDOff();
-      LEDStick1.setLEDColor(ledValue1, 255, 0, 0);
-    }
+    led_stick(LEDStick1, conState1, ledValue1);
     last_ledValue1 = ledValue1;
   }
 
-  // for (int i = 0; i <= 10; i++){
-  //   if(i == ledValue1){
-  //     LEDStick1.setLEDBrightness(i, 30);
-  //     LEDStick1.setLEDColor(i, 255, 0, 0);
-  //   }else{
-  //     LEDStick1.setLEDBrightness(i, 0);
-  //     //LEDSt1.setLEDColor(i, 0, 0, 0);
-  //   }
-  // }
-  Serial.print(potValue1);
+  //Serial.print(potValue1);
   Serial.print("\n");
 
   delay(1);
