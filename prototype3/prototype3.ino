a9e0ab4d7ab8a77a8e3db3f68ee9faa033a8143d
Sebastian Gilligan <sjgillig@bu.edu>
2025-02-20 16:54:44 -0500

    Update prototype3.ino

--- prototype3/prototype3.ino
+++ prototype3/prototype3.ino
@@ -1,12 +1,14 @@
 #include <Wire.h>
 #include <PWMServo.h>
+#include <Arduino.h>
+#include "TeensyThreads.h"
 #include "Qwiic_LED_Stick.h"
 
 #define SENS_COEF_1 30
 
 #define INITIAL_THRESH 1 
 #define WAITING_THRESH 50
-#define TOGGLE_THRESH 1400
+#define TOGGLE_THRESH 600
 
 typedef enum {
   WAITING, INITIAL_PRESS, TOGGLE,
@@ -39,30 +41,32 @@ int last_ledValue1 = 0;
 
 int maxDepres1 = 0;
 int minDepres1 = 0;
-int sens1 = 512; //min 0, max 1024
+int sens1 = 0; //min 0, max 1024
 int pos1 = 0;
 sustain_state_t susState1 = WAITING;
 
-control_state_t cycle_state(control_state_t state){
-  switch(state){
+void cycle_conState1(){
+  switch(conState1){
     case INIT:
-      return MIN;
+      conState1 = MIN;
+      break;
     case MIN:
-      return MAX;
+      conState1 = MAX;
+      break;
     case MAX:
-      return SENS;
+       conState1 = SENS;
+       break;
     default:
-      return MIN; 
+       conState1 = MIN;
+       break;
   }
+  led_stick(LEDStick1,conState1,ledValue1);
+  //delay(1000);
 }
 
-void get_sensor_inputs(){
-  
-}
 
-void get_pot_inputs(){
 
-}
+
 
 int get_led_value(int pot_value){
   int led_value;
@@ -132,10 +136,63 @@ void led_stick(LED &led, control_state_t state, int value){
   }
 }
 
+void sustain_control(){
+  if(susState1 == WAITING && sensorValue1 > 10){
+    susState1 = INITIAL_PRESS; //wating -> inital_press
+  }
+
+  if(susState1 == INITIAL_PRESS && sensorValue1 < 10){
+    susState1 = WAITING; //initial_press -> waiting
+  }
+
+  if(susState1 == INITIAL_PRESS && sensorValue1 > TOGGLE_THRESH){
+    susState1 = TOGGLE; //initial_press -> toggle
+  }
+
+  if(susState1 == TOGGLE && sensorValue1 > TOGGLE_THRESH){
+    //Serial.print("HERE");
+    pos1 = map(sensorValue1,0,1023,0,179);
+    servo1.write(pos1);
+    delay(1); 
+  }else{
+    susState1 = WAITING;
+  }
+
+
+
+}
+
+void get_sensor_inputs(){
+  sensorValue1 = analogRead(sensorInput1);
+  sensorValue1 = sensorValue1 * sens1;
+}
+
+void get_pot_inputs(){
+  static int old_sens1 = 512;
+  static int last_potValue1;
+  int loc_potValue1;
+
+  loc_potValue1 = analogRead(potInput1);
+  if(loc_potValue1 != last_potValue1){
+    potValue1 = loc_potValue1;
+    if(conState1 == SENS){
+      sens1 = log((potValue1 / SENS_COEF_1) + 1);
+      Serial.println(sens1);
+      old_sens1 = sens1;
+    }else{
+      sens1 = old_sens1;
+    }
+  }
+}
+
 void setup() {
   Wire.begin();
   Serial.begin(115200);
+
+  pinMode(buttonInput1,INPUT);
   servo1.attach(motorOutput1);
+
+  attachInterrupt(digitalPinToInterrupt(buttonInput1), cycle_conState1, RISING);
 
   if (LEDStick1.begin() == false){
     Serial.println("Qwiic LED Stick failed to begin. Please check wiring and try again!");
@@ -146,82 +203,22 @@ void setup() {
 }
 
 void loop() {
-  sensorValue1 = analogRead(sensorInput1);
-  sensorValue1 = sensorValue1 * sens1;
+  get_sensor_inputs();
+  get_pot_inputs();
 
-  potValue1 = analogRead(potInput1);
-
-  Serial.print(sensorValue1);
-  Serial.print("\n");
-
-  //actuate
-  if(susState1 == WAITING && sensorValue1 > 10){
-    susState1 = INITIAL_PRESS; //wating -> inital_press
-  }
-
-  if(susState1 == INITIAL_PRESS && sensorValue1 < 10){
-    susState1 = WAITING; //initial_press -> waiting
-  }
-
-  if(susState1 == INITIAL_PRESS && sensorValue1 > TOGGLE_THRESH){
-    while(1){
-      sensorValue1 = analogRead(sensorInput1);
-      sensorValue1 = sensorValue1 * sens1;
-      susState1 = TOGGLE; //initial_press -> toggle
-      pos1 = map(sensorValue1,0,1023,0,179);
-      servo1.write(pos1);
-
-      sens1 = log((potValue1 / SENS_COEF_1) + 1);
-      ledValue1 = get_led_value(sensorValue1);
-      Serial.print(sensorValue1);
-      Serial.print("\n");
-
-
-      delay(1); 
-
-
-
-    }
-  }
-
-  // if(susState1 == TOGGLE){
-  //   //delay(500);
-  //   susState1 = WAITING; //toggle -> waiting
-  //   //ITimer.stopTimer();
-  //   //timer_counter = 0;
-  // }
-
-  //button check (could be thread)
-  buttonState1 = digitalRead(buttonInput1);
+  //Serial.println(sensorValue1);
+  sustain_control();
   
-  if(buttonState1 == HIGH){
-    conState1 = cycle_state(conState1);
-    Serial.print(conState1);
-    Serial.print("\n");
-    led_stick(LEDStick1,conState1,ledValue1);
-    delay(1000);
-  }
-
   if(conState1 == SENS){
-    sens1 = log((potValue1 / SENS_COEF_1) + 1);
     ledValue1 = get_led_value(sensorValue1);
-    Serial.print(sensorValue1);
-    Serial.print("\n");
   }else{
-    potValue1 = analogRead(potInput1);
     ledValue1 = get_led_value(potValue1);
   }
-  
-  if(last_ledValue1 != ledValue1){
-    Serial.print(ledValue1);
-    Serial.print("\n");
-    led_stick(LEDStick1,conState1,ledValue1);
 
+  if(last_ledValue1 != ledValue1){
+    led_stick(LEDStick1,conState1,ledValue1);
     last_ledValue1 = ledValue1;
   }
-
-
-  //Serial.print(potValue1);
 
 
   delay(1);
